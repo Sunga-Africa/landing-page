a62d55b36e3d4608dfd54e6e70baefb2e8d350cd
Christian Jombo <unclechris4life@gmail.com>
2026-04-16 19:37:01 +0100

    chore: add project configuration and dependencies

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+            },
+        ],
+        domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+    },
+};
+
+export default nextConfig;
